b305f711cec8eea5d515fd40054c53380d40eb3f
josephg <jmgarcia214@yahoo.com>
2020-06-24 08:30:36 +0800

    * Remove un necessary controller method. * Added index.html to directly access page on localhost:8080

--- src/main/java/com/pccw/herokuview/web/controller/PageController.java
+++ src/main/java/com/pccw/herokuview/web/controller/PageController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/view")
 public class PageController {
+
+    @GetMapping("/")
+    public String defaultMapping(){
+        return "index";
+    }
 
 }
